a0dcfc34ef9721e02eec83686f770a3fcb75ff4a
Kiran Supekar <164185750+Ksupekar89@users.noreply.github.com>
2024-07-27 16:35:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var tl=gsap.timeline();
+tl.from(".left-nav img",{
+    y:30,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4
+})
+
+var tl=gsap.timeline();
+tl.from(".right-nav button",{
+    y:30,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4
+})
+
+
+// var tl=gsap.timeline();
+// tl.from(".TV-shows .Unlimited h1",{
+//     y:100,
+//     opacity:8,
+//     duration:1,
+//     stagger:9
+// })
+
+var tl=gsap.timeline();
+tl.from(".email-o ",{
+    x:-40,
+    opacity:0,
+    duration:0.9,
+    stagger:0.5
+})
+
+var tl=gsap.timeline();
+tl.from(".get-started ",{
+    x:40,
+    opacity:0,
+    duration:0.8,
+    stagger:0.5
+})
